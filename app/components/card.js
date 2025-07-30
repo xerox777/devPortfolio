eef90c47db19bd65c43b96132a8ef5e74d952e15
firepaw10 <darkfirepaw10@hotmail.com>
2025-07-30 13:02:26 -0700

    Working

--- app/components/card.js
+++ app/components/card.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Card = ({ media, title, description, link }) => {
   const isVideo = media?.endsWith('.mp4');
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedImg, setSelectedImg] = useState("");
 
+  const openModal = (imgSrc) => {
+    setSelectedImg(imgSrc);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setSelectedImg("");
+  };
+  // 🔑 Handle Escape key
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        closeModal();
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener("keydown", handleKeyDown);
+    }
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen]);
   return (
     <div className="max-w-sm bg-white rounded-2xl overflow-hidden shadow hover:shadow-lg transition-shadow duration-300">
       {isVideo ?
@@ -12,7 +39,19 @@ const Card = ({ media, title, description, link }) => {
         className="w-full h-48 object-cover"/>
       ) : (
 
-        <img src={media} alt={title} className="w-full h-48 object-cover" />
+        <img src={media} alt={title} className="w-full h-48 object-cover" onClick={() => openModal(media)}/>
+      )}
+      {isOpen && (
+
+      <div className="min-w-screen min-h-screen fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50"
+        onClick={closeModal} >
+          <div  className="max-w-4xl max-h-[90vh] p-4"
+            onClick={(e) => e.stopPropagation()}>
+
+        <img src={selectedImg} alt={title} className="rounded-lg shadow-lg object-contain max-h-[80vh] transform scale-125 transition duration-300" onClick={() => openModal(media)}/>
+            </div>
+
+      </div>
       )}
       <div className="p-4">
         <h3 className="text-lg font-bold text-gray-900">{title}</h3>
